b3723f8ce18ee5432ee09a1bc2d6fe66663059c8
mehul <mailtomehulraj@gmail.com>
2025-08-10 21:14:50 +0530

    origins changes to *

--- backend/server.js
+++ backend/server.js
@@ -9,22 +9,7 @@ dotenv.config({ debug: true });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-const allowedOrigins = [
-    "http://localhost:5173",
-    "https://url-shortner-xi-steel.vercel.app/",
-];
-
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true
-}));
+app.use(cors({ origin: "*" }));
 
 app.use(express.json());
 app.use(morgan("dev"));
